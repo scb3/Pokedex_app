227c6933ff23eb2f0b36af0a1672fc90251246cc
scb3 <scott.c.brooks@outlook.com>
2021-11-30 15:04:04 +0000

    Pokemons! now shows on Screen as a 500 limit list

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import React, {component, Component} from 'react';
-import { render } from '@testing-library/react';
+import React, { Component } from "react";
+
 
 class App extends Component {
-   constructor(props) {
-      super(props) ;
-
-         this.state = {
-            items : [ ],
-            isLoaded : false,
-         }
+constructor(props) {
+super(props);
+this.state = {
+items: [],
+isLoaded: false
+};
 }
 
-// https://pokeapi.co/api/v2/pokemon/
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=500")
+    .then((res) => res.json())
+    .then((json) => {
+    this.setState({
+    isLoaded: true,
+    items: json
+    });
+    });
+    }
 
-componentDidMount() {
-   fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json() ) .then(json => {
-      this.setState ({
-         isLoaded: true,
-         items: json,
-      })
-   });
-}
 
-render() {
-   var{isLoaded,items}= this.state;
-   if(!isLoaded) {
-      return <div> Loading....</div>
-   }
-   else {
-      return (
-         <div className='App'> 
-            <ul>
-               {items.map(items => (
-                   <li key = {items.id }>
-                     {items.name}| {items.email}
-                  </li>
-               ))}
-            </ul>
-         </div>
-      );
-   }
-}
-
-}
+  render() {
+    var { isLoaded, items } = this.state;
+    console.log(items)
+    if (!isLoaded) {
+    return <div> Loading....</div>;
+    } else {
+    return (
+    <div className="App">
+    <ul>
+    {items.results.map((item) => (
+    <li key={item.id}>
+    {item.name}
+    </li>
+    ))}
+    </ul>
+    </div>
+    );
+    }
+    }
+    }
+   
 export default App;
